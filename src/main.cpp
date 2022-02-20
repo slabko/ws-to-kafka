deb6ba7e111d779ac682daad0fb75932e83dc995
Andrew Slabko <andrew@slabko.com>
2022-02-20 17:06:28 +0000

    add spdlog 1.9.2

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,18 @@
 #include <chrono>
 #include <iostream>
 #include <librdkafka/rdkafkacpp.h>
+#include <spdlog/spdlog.h>
 
 using namespace std::chrono_literals;
 
 int main() {
-  std::cout << "starting" << std::endl;
+  spdlog::info("starting");
 
   auto conf = RdKafka::Conf::create(RdKafka::Conf::CONF_GLOBAL);
   std::string errstr;
 
-  if (conf->set("bootstrap.servers", "localhost:9092", errstr) != RdKafka::Conf::CONF_OK) {
+  if (conf->set("bootstrap.servers", "localhost:9092", errstr) !=
+      RdKafka::Conf::CONF_OK) {
     throw std::runtime_error(errstr);
   }
 
@@ -21,8 +23,7 @@ int main() {
   boost::asio::steady_timer timer(ioc);
   timer.expires_from_now(1s);
 
-  timer.async_wait(
-      [](boost::system::error_code ec) { std::cout << "done" << std::endl; });
+  timer.async_wait([](boost::system::error_code ec) { spdlog::info("done"); });
 
   ioc.run();
 
