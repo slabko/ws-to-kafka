fa4a6721ec6fc2a0c0bb891150027e812d90630e
Andrew Slabko <andrew@slabko.com>
2022-03-13 22:33:31 +0000

    draft implementation

--- src/pch.h
+++ src/pch.h
@@ -6,6 +6,7 @@
 #include <memory>
 #include <string>
 #include <thread>
+#include <vector>
 
 #include <boost/asio.hpp>
 #include <boost/asio/ssl.hpp>
--- src/s3sink/main.cpp
+++ src/s3sink/main.cpp
@@ -1,10 +1,17 @@
 #include "../pch.h"
 
+#include "message_store.hpp"
 #include <ostream>
 #include <sstream>
+#include <aws/core/Aws.h>
+
+using slabko::s3sink::MessageStore;
 
 int main()
 {
+  Aws::SDKOptions options;
+  options.loggingOptions.logLevel = Aws::Utils::Logging::LogLevel::Debug;
+  Aws::InitAPI(options);
 
   auto conf = std::unique_ptr<RdKafka::Conf>(
     RdKafka::Conf::create(RdKafka::Conf::CONF_GLOBAL));
@@ -25,6 +32,11 @@ int main()
     return 0;
   }
 
+  if (conf->set("auto.offset.reset", "earliest", errstr) != RdKafka::Conf::CONF_OK) {
+    spdlog::critical(errstr);
+    return 0;
+  }
+
   RdKafka::KafkaConsumer* consumer = RdKafka::KafkaConsumer::create(conf.get(), errstr);
   if (!consumer) {
     spdlog::critical("Failed to create consumer: {}", errstr);
@@ -38,61 +50,43 @@ int main()
     return 1;
   }
 
+  MessageStore message_store { 10 * 1025 * 1024 };
   while (true) {
-    auto str = std::stringstream();
-    std::unique_ptr<RdKafka::Message> msg;
-    int64_t block_offset {};
-    int64_t block_timestamp {};
-    bool block_is_running { false };
-
-    for (int i = 0; i < 200;) {
-      msg = std::unique_ptr<RdKafka::Message> { consumer->consume(1000) };
-      switch (msg->err()) {
-      case RdKafka::ERR__TIMED_OUT:
-        break;
-      case RdKafka::ERR_NO_ERROR: {
-        str << static_cast<const char*>(msg->payload()) << "\n";
-        ++i;
-
-        if (!block_is_running) {
-          block_offset = msg->offset();
-
-          RdKafka::MessageTimestamp ts = msg->timestamp();
-          block_timestamp = ts.timestamp;
-
-          block_is_running = true;
-
-          spdlog::info("New block [{}] {}", block_timestamp, block_offset);
-        }
-
-        break;
-
-        // spdlog::info("Message [{}] {} - {}", ts.timestamp, *message->key(), static_cast<const char*>(message->payload()));
+    auto msg = std::unique_ptr<RdKafka::Message> { consumer->consume(1000) };
+    switch (msg->err()) {
+    case RdKafka::ERR__TIMED_OUT:
+      break;
+    case RdKafka::ERR_NO_ERROR: {
+      auto ts = msg->timestamp().timestamp;
+      if (!message_store.IsRunningCheckpoint()) {
+        auto offset = msg->offset();
+        std::string filename { std::to_string(ts) + "_" + std::to_string(offset) + ".txt" };
+        message_store.BeginCheckpoint(filename);
       }
-      default:
-        spdlog::critical("Consume failed: {}", msg->errstr());
-        return 0;
-      }
-    }
 
-    {
-      std::string filename { std::to_string(block_timestamp) + "_" + std::to_string(block_offset) + ".txt" };
-      std::ofstream file { filename };
-      if (file.is_open()) {
-        file << str.rdbuf();
-        file.close();
+      using namespace std::string_literals;
+      spdlog::info("Message with size {}", msg->len());
+      message_store.Push(std::to_string(ts));
+      message_store.Push(" "s);
+      message_store.Push(static_cast<const char*>(msg->payload()), msg->len());
+      message_store.Push("\n"s);
+      spdlog::info("Store size is {}", message_store.Size());
+
+      if (message_store.Size() > (1024 * 1024)) {
+        spdlog::info("Commiting checkpoint");
+        message_store.CommitCheckpoint();
         consumer->commitSync(msg.get());
-        str = std::stringstream();
-
-        block_offset = 0;
-        block_timestamp = 0;
-        block_is_running = false;
-      } else {
-        spdlog::critical("Failed to create file {}", filename);
-        return 1;
       }
+
+      break;
+    }
+    default:
+      spdlog::critical("Consume failed: {}", msg->errstr());
+      return 0;
     }
   }
+
+  Aws::ShutdownAPI(options);
 
   return 0;
 }
